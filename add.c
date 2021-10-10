e5adf81d29536cbd6caa401496076a10760241a4
Salman Ahmad Khan <90852121+Salman-Ahmad-Khan@users.noreply.github.com>
2021-10-10 20:39:24 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// #define fun(x) (x*x-x)
+// int main()
+// {
+// float i;
+// i=37.0/fun(2);
+// printf("%.2f",i);
+
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+
+return 0;
+
+}
