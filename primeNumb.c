1a40396c648f5dae792459efc1992d785ce6042f
Salman Ahmad Khan <90852121+Salman-Ahmad-Khan@users.noreply.github.com>
2021-10-11 18:01:48 +0530

    Update primeNumb.c

--- primeNumb.c
+++ primeNumb.c
@@ -1,3 +1,5 @@
+//c program to check a given number is prime or not.
+
 #include <stdio.h>
 void main()
 {
@@ -12,20 +14,6 @@ void main()
             printf("%d is PRIME number\n", n);
         else
             printf("%d is  NOT a Prime number\n", n);
-    // }
+    }
 
-    // int a, i;
-
-    // printf("Enter a number\n");
-    // scanf("%d", &a);
-    // for (i = 2; i  <= a - 1; i++)
     
-    //     if (a % i == 0)
-    //     break;
-
-    //         if (i == a)
-    //             printf("%d is a prime number",a);
-    //         else
-    //             printf("%d is  not a prime number",a);
-    
-}
